2809134b8b411033ce91a12dbc3fc06dbf792bc2
Zach Ohara <zjohara@gmail.com>
2015-08-29 13:40:42 -0700

    Add a basic TCompoundOpMode implementation

--- src/io/github/thunderbots/sdk/opmode/TCompoundOpMode.java
+++ src/io/github/thunderbots/sdk/opmode/TCompoundOpMode.java
@@ -5,6 +5,34 @@ package io.github.thunderbots.sdk.opmode;
  *
  * @author Zach Ohara
  */
-public class TCompoundOpMode {
+public class TCompoundOpMode extends TLinearOpMode {
+	
+	private TLinearOpMode[] opmodes;
+	
+	public TCompoundOpMode(TLinearOpMode[] opmodes) {
+		this.opmodes = opmodes;
+	}
+
+	@Override
+	protected void initializeRobot() {
+		for (TLinearOpMode opmode : this.opmodes) {
+			opmode.initializeRobot();
+		}
+	}
+
+	@Override
+	protected void main() {
+		for (TLinearOpMode opmode : this.opmodes) {
+			opmode.start();
+		}
+		while (this.opModeIsActive()) {
+			for (TLinearOpMode opmode : this.opmodes) {
+				opmode.loop();
+			}
+		}
+		for (TLinearOpMode opmode : this.opmodes) {
+			opmode.stop();
+		}
+	}
 
 }
