dbcf287f3dc705f18c357afe19f2eb08e10d0e98
Zach Ohara <zjohara@gmail.com>
2015-08-31 19:09:32 -0700

    Add experimental task scheduler

---
+++ src/io/github/thunderbots/sdk/scheduler/TaskScheduler.java
@@ -0,0 +1,44 @@
+package io.github.thunderbots.sdk.scheduler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 
+ *
+ * @author Zach Ohara
+ */
+public class TaskScheduler {
+	
+	private volatile List<Runnable> tasks;
+	
+	private Thread taskThread;
+	
+	public TaskScheduler() {
+		this.tasks = new ArrayList<Runnable>();
+		this.taskThread = new TaskSchedulerThread();
+		this.taskThread.start();
+	}
+	
+	public void scheduleTask(Runnable task) {
+		this.tasks.add(task);
+	}
+	
+	private void runAllTasks() {
+		for (Runnable r : this.tasks) {
+			r.run();
+		}
+	}
+	
+	private class TaskSchedulerThread extends Thread {
+
+		@Override
+		public void run() {
+			while (!this.isInterrupted()) {
+				TaskScheduler.this.runAllTasks();
+			}
+		}
+		
+	}
+
+}
