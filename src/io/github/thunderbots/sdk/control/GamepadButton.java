521a957d1180a1beeb883bddf1c88142aafd4fe3
Pranav Mathur <pranavmathur99@gmail.com>
2015-09-02 17:18:28 -0700

    Add GamepadButton Enumeration

---
+++ src/io/github/thunderbots/sdk/control/GamepadButton.java
@@ -0,0 +1,23 @@
+package io.github.thunderbots.sdk.control;
+
+/**
+ * @author Pranav Mathur
+ */
+public enum GamepadButton {
+	
+	A,
+	B,
+	X,
+	Y,
+	UP,
+	DOWN,
+	LEFT,
+	RIGHT,
+	LEFTBUMPER,
+	RIGHTBUMPER,
+	LEFTSTICK,
+	RIGHTSTICK,
+	LEFTTRIGGER,
+	RIGHTTRIGGER
+	
+}
