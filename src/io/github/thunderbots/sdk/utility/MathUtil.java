20a9ea67d9183b5d9a05e11b749310e56ed6d037
Pranav Mathur <pranavmathur99@gmail.com>
2015-09-03 23:21:08 -0700

    Refactor parameter name in scaleToRange()

--- src/io/github/thunderbots/sdk/utility/MathUtil.java
+++ src/io/github/thunderbots/sdk/utility/MathUtil.java
@@ -8,7 +8,7 @@ public class MathUtil {
 	 * input range, 0 will be returned. If the absolute value of the number is
 	 * above the input range, the maximum value of the output range, but with
 	 * the same polarity as the number, will be returned. 
-	 * @param d the number to be scaled
+	 * @param num the number to be scaled
 	 * @param inputRange a two-element array representing the lowest and
 	 * and highest values, respectively, of the range to be scaled from 
 	 * @param outputRange a two-element array representing the lowest and
@@ -16,16 +16,16 @@ public class MathUtil {
 	 * @return the number, scaled to the same relative position within the
 	 * output range
 	 */
-	public static double scaleToRange(double d, double[] inputRange, double[] outputRange) {
-		double signum = Math.signum(d);
-		d = Math.abs(d);
-		if (d < inputRange[0])
+	public static double scaleToRange(double num, double[] inputRange, double[] outputRange) {
+		double signum = Math.signum(num);
+		num = Math.abs(num);
+		if (num < inputRange[0])
 			return 0;
-		if (d > inputRange[1])
+		if (num > inputRange[1])
 			return outputRange[1] * signum;
 		double inputDifference = inputRange[1] - inputRange[0];
 		double outputDifference = outputRange[1] - outputRange[0];
-		double posInRange = (d - inputRange[0]) / inputDifference;
+		double posInRange = (num - inputRange[0]) / inputDifference;
 		double posOutRange = outputRange[0] + (posInRange * outputDifference);
 		return posOutRange * signum;
 	}
