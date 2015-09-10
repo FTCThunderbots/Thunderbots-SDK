f0cb50dee817828a312610dd24fd92e9adc15703
Zach Ohara <zjohara@gmail.com>
2015-09-09 22:20:35 -0700

    Add docs for DriveMotorSet

--- src/io/github/thunderbots/lightning/drive/DriveMotorSet.java
+++ src/io/github/thunderbots/lightning/drive/DriveMotorSet.java
@@ -22,12 +22,23 @@ import io.github.thunderbots.lightning.Lightning;
 import io.github.thunderbots.lightning.hardware.TMotor;
 
 /**
+ * A {@code DriveMotorSet} is a collection of motors used for the sole purpose of driving the
+ * robot.
+ * 
  * @author Zach Ohara
  */
 public class DriveMotorSet {
 
+	/**
+	 * The array of motors that this {@code DriveMotorSet} comprises.
+	 */
 	private TMotor[] motors;
 
+	/**
+	 * Constructs a new {@code DriveMotorSet} using the motors with the given names.
+	 *
+	 * @param names the names of all the motors to use with this motor set.
+	 */
 	public DriveMotorSet(String[] names) {
 		this.motors = new TMotor[names.length];
 		for (int i = 0; i < names.length; i++) {
@@ -35,6 +46,15 @@ public class DriveMotorSet {
 		}
 	}
 
+	/**
+	 * Set the power of the motors in this motor set from the corresponding power values
+	 * in the given double array. For example, the motor at {@code motors[n]} will be
+	 * assigned the power value at {@code powers[n]}. If the lengths of {@code motors}
+	 * and {@code powers} are mismatched, then all the available pairs will be matched, and
+	 * the extra values will be discarded.
+	 *
+	 * @param powers the power values to assign to the corresponding motors.
+	 */
 	public void setMotorPowers(double[] powers) {
 		Lightning.sendTelemetryData("Motors", Arrays.toString(powers));
 		int motorSet = Math.min(powers.length, this.motors.length);
