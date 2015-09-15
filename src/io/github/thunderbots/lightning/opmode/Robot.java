0944e7062b63212c6e7b346757702216fde48e17
Zach Ohara <zjohara@gmail.com>
2015-09-14 21:32:03 -0700

    Add documentation for Robot

--- src/io/github/thunderbots/lightning/opmode/Robot.java
+++ src/io/github/thunderbots/lightning/opmode/Robot.java
@@ -1,7 +1,17 @@
 package io.github.thunderbots.lightning.opmode;
 
+/**
+ * {@code Robot} is an interface that should be implemented by any class representing a
+ * physical robot. In the future, this interface will be used to provide more seamless
+ * operation between op modes objects and robot objects.
+ */
 public interface Robot {
 
-	void initializeRobot();
+	/**
+	 * Initializes the robot. The implementation of this method should be used in place
+	 * of a constructor. Instance variables for a robot object should initialize from
+	 * the available hardware maps in this method.
+	 */
+	public void initializeRobot();
 
 }
