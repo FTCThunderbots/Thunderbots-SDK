5cbccbf0baf1ebb7c9572b4b7e89aa629aa66b21
Zach Ohara <zjohara@gmail.com>
2015-09-14 21:09:27 -0700

    Add documentation for CompoundOpMode

--- src/io/github/thunderbots/lightning/opmode/CompoundOpMode.java
+++ src/io/github/thunderbots/lightning/opmode/CompoundOpMode.java
@@ -1,16 +1,31 @@
 package io.github.thunderbots.lightning.opmode;
 
 /**
+ * A {@code CompoundOpMode} is an op mode that includes the functionality of several
+ * different op modes.
+ * 
  * @author Zach Ohara
  */
 public class CompoundOpMode extends LightningOpMode {
 
+	/**
+	 * The list of op modes that this compound op mode combines the functionality of.
+	 */
 	private LightningOpMode[] opmodes;
 
+	/**
+	 * Constructs a new compound op mode with the given list of op modes.
+	 *
+	 * @param opmodes the list of op modes to be combined.
+	 * @see #opmodes
+	 */
 	public CompoundOpMode(LightningOpMode[] opmodes) {
 		this.opmodes = opmodes;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	protected void initializeRobot() {
 		for (LightningOpMode opmode : this.opmodes) {
@@ -18,6 +33,9 @@ public class CompoundOpMode extends LightningOpMode {
 		}
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	protected void main() {
 		for (LightningOpMode opmode : this.opmodes) {
