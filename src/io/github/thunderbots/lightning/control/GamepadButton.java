f8b5b8658131034e3364c5a0c01bb4778c4a3c40
Zach Ohara <zjohara@gmail.com>
2015-09-15 21:52:27 -0700

    Add copyright statements

--- src/io/github/thunderbots/lightning/control/GamepadButton.java
+++ src/io/github/thunderbots/lightning/control/GamepadButton.java
@@ -1,8 +1,25 @@
+/* Copyright (C) 2015-2016 Thunderbots Robotics
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package io.github.thunderbots.lightning.control;
 
 /**
  * @deprecated There is currently no purpose for this enumeration. It will be un-deprecated
  * if a purpose can be found
+ * 
  * @author Pranav Mathur
  */
 public enum GamepadButton {
