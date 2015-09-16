ef5ee288850929c5adcd51e1e4a85205c3eeca27
Zach Ohara <zjohara@gmail.com>
2015-09-15 17:04:34 -0700

    Add documentation for OpMode annotation

--- src/io/github/thunderbots/annotation/OpMode.java
+++ src/io/github/thunderbots/annotation/OpMode.java
@@ -2,8 +2,19 @@ package io.github.thunderbots.annotation;
 
 import java.lang.annotation.Documented;
 
+/**
+ * The {@code OpMode} annotation should be added to all op modes that appear in the
+ * op mode list. It details information about the type of op mode, such as
+ * "teleop" or "autonomous", or anything else that the client class may define. This
+ * information is not currently used. It also includes the name of the op mode, and
+ * this name will eventually be used to present the op mode in the list.
+ * 
+ * @author Pranav Mathur
+ */
 @Documented
 public @interface OpMode {
+	
 	String type();
 	String name();
+	
 }
