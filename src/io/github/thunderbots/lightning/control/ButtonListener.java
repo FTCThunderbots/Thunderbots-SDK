e4dd5b949930b46144ba0000a1068ed5dc9c3a9b
Zach Ohara <zjohara@gmail.com>
2015-09-27 18:01:32 -0700

    Add a ButtonListener interface

---
+++ src/io/github/thunderbots/lightning/control/ButtonListener.java
@@ -0,0 +1,11 @@
+package io.github.thunderbots.lightning.control;
+
+/**
+ * The {@code JoystickListener} interface is a functionally empty interface that should be
+ * implemented by any object that can respond to joystick button presses.
+ *
+ * @author Zach Ohara
+ */
+public interface ButtonListener {
+
+}
