bfe9cff28d4b67955fc83567e6cf4f53101ed38d
Zach Ohara <zjohara@gmail.com>
2015-10-31 13:19:57 -0700

    Implemented getForwardPower() and getClockwisePower() in TankControlLayout
    The methods function as described in the javadoc of the `TankControlLayout` class.

--- src/io/github/thunderbots/lightning/control/layout/TankControlLayout.java
+++ src/io/github/thunderbots/lightning/control/layout/TankControlLayout.java
@@ -16,9 +16,7 @@
 
 package io.github.thunderbots.lightning.control.layout;
 
-import io.github.thunderbots.lightning.Lightning;
 import io.github.thunderbots.lightning.control.Joystick;
-import io.github.thunderbots.lightning.utility.MathUtil;
 
 /**
  * A {@code TankControlLayout} is a {@code ControlLayout} that represents a tank-like
@@ -35,43 +33,15 @@ import io.github.thunderbots.lightning.utility.MathUtil;
  * thumbsticks.
  */
 public class TankControlLayout implements ControlLayout {
-	
-	/**
-	 * Returns the motion of the robot when in Tank mode
-	*/
-	
-	protected void tankMovement() {
-		
-		/**
-		 * Gets drivingGamepad from Joystick
-		 */
-		
-		Joystick drivingGamepad = Lightning.getJoystick(1);
-		
-		/**
-		 * Returns the motion and magnitude of that motion when rightStickY and leftStickY is greater than 0
-		 * 
-		 * MathUtil.average finds the average of the Y values of the sticks.
-		 * This will be used as the magnitude of 
-		 */
-		
-		if (drivingGamepad.rightStickY() > 0 && drivingGamepad.leftStickY() > 0) {
-			//forward power is average is both of them 
-			double average = MathUtil.average(drivingGamepad.rightStickY(), drivingGamepad.leftStickY());
-			return;
-			
-		}
-	}
 
 	@Override
 	public double getForwardPower(Joystick joy) {
-		// TODO Auto-generated method stub
-		return 0;
+		return (joy.leftStickY() + joy.rightStickY()) / 2;
 	}
 
 	@Override
 	public double getClockwisePower(Joystick joy) {
-		// TODO Auto-generated method stub
-		return 0;
+		return (joy.leftStickY() - joy.rightStickY()) / 2;
 	}
+
 }
