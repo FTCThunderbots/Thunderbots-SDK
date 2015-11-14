ce8f5270a98f59fb4167c7bce38971a5a2b0010d
Zach Ohara <zjohara@gmail.com>
2015-11-14 14:18:48 -0800

    Remove non-buttons from the JoystickButton enumeration
    Non-buttons may be tracked with an event system if they can be translated into 'pressed' or 'not pressed', but this is something to be revisted in later development.

--- src/io/github/thunderbots/lightning/control/JoystickButton.java
+++ src/io/github/thunderbots/lightning/control/JoystickButton.java
@@ -2,10 +2,6 @@ package io.github.thunderbots.lightning.control;
 
 /**
  * The {@code JoystickButton} enumeration contanis all buttons that can be pressed on the joysticks.
- * It also contains controls on the joystick which are non-binary, but that can be treated as
- * buttons. For example, the position of the thumb sticks are given as a decimal value between -1
- * and 1, with 0 being the neutral position. The thumb stick is considered 'pressed' if its value
- * is either -1 or 1.
  *
  * @author Zach Ohara
  * @author Pranav Mathur
@@ -26,20 +22,7 @@ public enum JoystickButton {
 	RIGHT_STICK,
 	
 	LEFT_BUMPER,
-	LEFT_TRIGGER,
-	
-	RIGHT_BUMPER,
-	RIGHT_TRIGGER,
-	
-	LEFT_X_NEG,
-	LEFT_X_POS,
-	LEFT_Y_NEG,
-	LEFT_Y_POS,
-	
-	RIGHT_X_NEG,
-	RIGHT_X_POS,
-	RIGHT_Y_NEG,
-	RIGHT_Y_POS;
+	RIGHT_BUMPER;
 	
 	@Override
 	public String toString() {
