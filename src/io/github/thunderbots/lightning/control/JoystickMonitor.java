443718136ffe4625e0e075d09d77ee4d8e688caf
Pranav Mathur <pranavmathur99@gmail.com>
2015-11-16 12:02:29 -0800

    Line Wrap Comments to <90 characters

--- src/io/github/thunderbots/lightning/control/JoystickMonitor.java
+++ src/io/github/thunderbots/lightning/control/JoystickMonitor.java
@@ -35,8 +35,8 @@ public class JoystickMonitor {
 	 * The joystick to monitor.
 	 */
 	/* (non-Javadoc)
-	 * If at all possible in the future, this should be a reference to a Joystick object, not just
-	 * an ID number.
+	 * If at all possible in the future, this should be a reference to a
+	 * Joystick object, not just an ID number.
 	 */
 	private int joystick;
 	
@@ -99,7 +99,8 @@ public class JoystickMonitor {
 	}
 	
 	/**
-	 * Invokes the given method on the given instance, and catches all exceptions.
+	 * Invokes the given method on the given instance, and catches all
+	 * exceptions.
 	 *
 	 * @param method the method to call.
 	 * @param instance the instance to call the method on.
