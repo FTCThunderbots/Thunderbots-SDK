1ea9b242dd16c042934f23e5a81d47efc1a54109
Pranav Mathur <pranavmathur99@gmail.com>
2015-11-20 20:26:34 -0800

    Add Alliance enumeration

---
+++ src/io/github/thunderbots/lightning/utility/Alliance.java
@@ -0,0 +1,14 @@
+package io.github.thunderbots.lightning.utility;
+
+public enum Alliance {
+	
+	BLUE (1),
+	RED (-1);
+	
+	private final int side;
+	
+	Alliance(int side) {
+		this.side = side;
+	}
+
+}
